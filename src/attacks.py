1ec505283c56813770d5e29e46e9279969b9ff4e
Abhinav Garg <abhinav.30garg@gmail.com>
2021-06-27 20:21:43 -0500

    formatting

--- src/attacks.py
+++ src/attacks.py
@@ -4,13 +4,6 @@ from src.cadv.util import compute_class, forward, compute_loss, get_colorization
 from src.cadv.dataloader import im_dataset
 import config.config as exp_config
 import torch
-import torchvision.models as models
-from torchvision.utils import save_image
-import torchvision.transforms as transforms
-from PIL import Image
-import numpy as np
-import argparse
-import json
 
 
 def fgsm(images, labels, model, criterion, device, attack_params=dict()):
@@ -55,17 +48,17 @@ def cadv(images, target, segmentation_model, criterion, device, attack_params=di
     model = color_net().to(device=device).eval()
     model.load_state_dict(torch.load(os.path.join(exp_config.log_root, 'latest_net_G.pth')))
 
-    threshold = 1e-5 # stopping criteria
+    threshold = 1e-5  # stopping criteria
     prev_diff = 0
     adv_images = []
     for i, (image) in enumerate(dataset_loader):
         im = image.to(device=device)
         attack_params['target'] = target
-        
+
         # Prepare hints, mask, and get current classification
         data = get_colorization_data(im, attack_params, model, device)
         optimizer = torch.optim.Adam([data['hints'].requires_grad_(), data['mask'].requires_grad_()],
-                                      lr=attack_params['lr'], betas=(0.9, 0.999))
+                                     lr=attack_params['lr'], betas=(0.9, 0.999))
 
         for itr in range(attack_params['num_iter']):
             out_rgb, y = forward(model, segmentation_model, attack_params, data)
@@ -75,24 +68,26 @@ def cadv(images, target, segmentation_model, criterion, device, attack_params=di
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             diff = val[0, 0, :, :] - val[0, 1, :, :]
 
-            if (itr+1) % 100 == 0 or itr == 0:
-                print(f'[{itr+1}/{attack_params["num_iter"]}] Loss: {loss:.3f}, ' 
-                      f'{torch.mean(diff).item():.5f}, {torch.mean((diff-prev_diff).abs()).item():.5f}')
-                
+            if (itr + 1) % 100 == 0 or itr == 0:
+                print(f'[{itr + 1}/{attack_params["num_iter"]}] Loss: {loss:.3f}, '
+                      f'{torch.mean(diff).item():.5f}, {torch.mean((diff - prev_diff).abs()).item():.5f}')
+
             if attack_params['targeted']:
-                if torch.allclose(torch.argmax(val, dim=1), target) and torch.mean(diff).item() > threshold and torch.mean((diff-prev_diff).abs()).item() < 1e-4:
-                    print(f'[{itr+1}/{attack_params["num_iter"]}] Loss: {loss:.3f}, ' 
-                      f'{torch.mean(diff).item():.5f}, {torch.mean((diff-prev_diff).abs()).item():.5f}')
+                if torch.allclose(torch.argmax(val, dim=1), target) and torch.mean(
+                        diff).item() > threshold and torch.mean((diff - prev_diff).abs()).item() < 1e-4:
+                    print(f'[{itr + 1}/{attack_params["num_iter"]}] Loss: {loss:.3f}, '
+                          f'{torch.mean(diff).item():.5f}, {torch.mean((diff - prev_diff).abs()).item():.5f}')
                     break
             else:
-                if not torch.allclose(torch.argmax(val, dim=1), target) and torch.mean(diff).item() > threshold and torch.mean((diff-prev_diff).abs()).item() < 1e-4:
-                    print(f'[{itr+1}/{attack_params["num_iter"]}] Loss: {loss:.3f}, ' 
-                      f'{torch.mean(diff).item():.5f}, {torch.mean((diff-prev_diff).abs()).item():.5f}')
+                if not torch.allclose(torch.argmax(val, dim=1), target) and torch.mean(
+                        diff).item() > threshold and torch.mean((diff - prev_diff).abs()).item() < 1e-4:
+                    print(f'[{itr + 1}/{attack_params["num_iter"]}] Loss: {loss:.3f}, '
+                          f'{torch.mean(diff).item():.5f}, {torch.mean((diff - prev_diff).abs()).item():.5f}')
                     break
             prev_diff = diff
-        adv_images.append(out_rgb[0]) # assuming batch_size of 1
-        
+        adv_images.append(out_rgb[0])  # assuming batch_size of 1
+
     return torch.stack(adv_images)
