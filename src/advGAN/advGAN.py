a627079fdefea378603445ef6f4926218eb5ab18
Abhinav Garg <abhinav.30garg@gmail.com>
2021-07-11 00:16:04 -0500

    adding advGAN

---
+++ src/advGAN/advGAN.py
@@ -0,0 +1,188 @@
+import torch.nn as nn
+import torch
+import config.config as exp_config
+import models
+import torch.nn.functional as F
+import torchvision
+import numpy as np
+from scipy.stats import rice
+import os
+
+log_dir = os.path.join(exp_config.log_root, exp_config.experiment_name)
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+def get_thicker_perturbation(label, scale=0.5):
+    perturbed_y = np.squeeze(label).copy()
+    rows_with_3 = sorted(list(set(np.where(perturbed_y == 3)[0])))
+    for row in rows_with_3:
+        y = perturbed_y[row]
+        all_3 = np.where(y == 3)[0]
+        value, counts = np.unique(y[:all_3[0]], return_counts=True)
+        y[all_3[0]: min(all_3[0] + int(counts[np.where(value == 2)][0] * scale), all_3[-1] + 1)] = 2
+
+        all_3 = np.where(y == 3)[0]
+        if len(all_3) != 0:
+            value, counts = np.unique(y[all_3[-1]:], return_counts=True)
+            y[max(all_3[-1] - int(counts[np.where(value == 2)][0] * scale) + 1, all_3[0]):all_3[-1] + 1] = 2
+    return perturbed_y[np.newaxis, :]
+
+
+class AdvGAN_Attack:
+    def __init__(self,
+                 device,
+                 model,
+                 model_num_labels,
+                 image_nc,
+                 box_min,
+                 box_max,
+                 criterion,
+                 b=4.775):
+        output_nc = image_nc
+        self.device = device
+        self.model_num_labels = model_num_labels
+        self.model = model
+        self.input_nc = image_nc
+        self.output_nc = output_nc
+        self.box_min = box_min
+        self.box_max = box_max
+        self.b = b
+
+        self.gen_input_nc = image_nc
+        self.netG = models.Generator(self.gen_input_nc, image_nc).to(device)
+        self.netDisc = models.Discriminator(image_nc).to(device)
+
+        # initialize all weights
+        self.netG.apply(weights_init)
+        self.netDisc.apply(weights_init)
+
+        # initialize optimizers
+        self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
+                                            lr=0.001)
+        self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
+                                            lr=0.001)
+        self.BCELoss = nn.BCELoss()
+        self.model_criterion = criterion
+
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+
+    def get_rician_samples(self, img_shape):
+        rician_samples = torch.tensor(rice.rvs(self.b, size=img_shape),
+                                      device=self.device, dtype=torch.float32)
+        return rician_samples
+
+    def train_batch(self, x, labels):
+        # optimize D
+        for i in range(1):
+            perturbation = self.netG(x)
+
+            # add a clipping trick
+            perturbation = torch.clamp(perturbation, -0.3, 0.3)
+            adv_images = perturbation + x
+            adv_images = torch.clamp(adv_images, self.box_min, self.box_max)
+
+            self.optimizer_D.zero_grad()
+            rician_noise = self.get_rician_samples(perturbation.shape)
+            pred_real = self.netDisc(rician_noise)
+            # loss_D_real = F.mse_loss(pred_real, torch.ones_like(pred_real, device=self.device))
+            loss_D_real = self.BCELoss(pred_real, torch.ones_like(pred_real, device=self.device))
+            loss_D_real.backward()
+
+            pred_fake = self.netDisc(perturbation.detach())
+            # loss_D_fake = F.mse_loss(pred_fake, torch.zeros_like(pred_fake, device=self.device))
+            loss_D_fake = self.BCELoss(pred_fake, torch.zeros_like(pred_fake, device=self.device))
+            loss_D_fake.backward()
+            loss_D_GAN = loss_D_fake + loss_D_real
+            self.optimizer_D.step()
+
+        # optimize G
+        for i in range(1):
+            self.optimizer_G.zero_grad()
+
+            # cal G's loss in GAN
+            pred_fake = self.netDisc(perturbation.detach())
+            # loss_G_fake = F.mse_loss(pred_fake, torch.ones_like(pred_fake, device=self.device))
+            loss_G_fake = self.BCELoss(pred_fake, torch.ones_like(pred_fake, device=self.device))
+            loss_G_fake.backward(retain_graph=True)
+
+            # calculate perturbation norm
+            C = 0.1
+            # loss_perturb = torch.mean(torch.norm(perturbation.view(perturbation.shape[0], -1), 2, dim=1))
+            # loss_perturb = torch.max(loss_perturb - C, torch.zeros(1, device=self.device))
+            loss_perturb = torch.norm(perturbation.view(perturbation.shape[0], -1), 2, dim=1)
+            loss_perturb = torch.mean(torch.max(loss_perturb - C, torch.zeros(loss_perturb.shape, device=self.device)))
+
+            # cal adv loss
+            logits_model = self.model(adv_images)
+            # probs_model = F.softmax(logits_model, dim=1)
+            # onehot_labels = torch.eye(self.model_num_labels, device=self.device)[labels]
+            loss_adv = self.model_criterion(logits_model, labels)
+
+            # C&W loss function
+            # real = torch.sum(onehot_labels * probs_model, dim=1)
+            # other, _ = torch.max((1 - onehot_labels) * probs_model - onehot_labels * 10000, dim=1)
+            # zeros = torch.zeros_like(other)
+            # loss_adv = torch.max(real - other, zeros)
+            # loss_adv = torch.sum(loss_adv)
+
+            # maximize cross_entropy loss
+            # loss_adv = - F.mse_loss(logits_model, onehot_labels)
+            # loss_adv = - F.cross_entropy(logits_model, labels)
+
+            adv_lambda = 10
+            pert_lambda = 1
+            loss_G = adv_lambda * loss_adv + pert_lambda * loss_perturb
+            loss_G.backward()
+            self.optimizer_G.step()
+
+        return loss_D_GAN.item(), loss_G_fake.item(), loss_perturb.item(), loss_adv.item()
+
+    def train(self, train_dataloader, epochs):
+        for epoch in range(1, epochs + 1):
+
+            if epoch == 50:
+                self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
+                                                    lr=0.0001)
+                self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
+                                                    lr=0.0001)
+            if epoch == 80:
+                self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
+                                                    lr=0.00001)
+                self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
+                                                    lr=0.00001)
+            loss_D_sum = 0
+            loss_G_fake_sum = 0
+            loss_perturb_sum = 0
+            loss_adv_sum = 0
+            for i, data in enumerate(train_dataloader, start=0):
+                labels = data['label']
+                imgs = torch.reshape(data['image'], [data['label'].shape[0]] + [1] + list(exp_config.image_size))
+                adv_labels = get_thicker_perturbation(labels.clone().detach().cpu().numpy(), 1)
+                loss_D_batch, loss_G_fake_batch, loss_perturb_batch, loss_adv_batch = \
+                    self.train_batch(imgs, adv_labels)
+                loss_D_sum += loss_D_batch
+                loss_G_fake_sum += loss_G_fake_batch
+                loss_perturb_sum += loss_perturb_batch
+                loss_adv_sum += loss_adv_batch
+
+            # print statistics
+            num_batch = len(train_dataloader)
+            print("epoch %d:\nloss_D: %.3f, loss_G_fake: %.3f,\
+             \nloss_perturb: %.3f, loss_adv: %.3f, \n" %
+                  (epoch, loss_D_sum / num_batch, loss_G_fake_sum / num_batch,
+                   loss_perturb_sum / num_batch, loss_adv_sum / num_batch))
+
+            # save generator
+            if epoch % 10 == 0:
+                netG_file_name = os.path.join(log_dir, 'netG_epoch_' + str(epoch) + '.pth')
+                torch.save(self.netG.state_dict(), netG_file_name)
---
+++ src/advGAN/main.py
@@ -0,0 +1,74 @@
+import torch
+import torchvision.datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from advGAN import AdvGAN_Attack
+from models import MNIST_target_net
+from src import utils
+from src.model import UNet2D
+import config.config as exp_config
+import os
+from data import acdc_data
+
+use_cuda = True
+image_nc = 1
+epochs = 2
+batch_size = 1
+BOX_MIN = 0
+BOX_MAX = 1
+b = 4.775
+
+# Define what device we are using
+print("CUDA Available: ", torch.cuda.is_available())
+device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.3, 0.3, 0.3], dtype=torch.float32,
+                                                          device=device, requires_grad=False))
+
+targeted_model = UNet2D(nchannels=1, nlabels=4)
+log_dir = os.path.join(exp_config.log_root, exp_config.experiment_name)
+global_step = 0
+if os.path.exists(log_dir):
+    targeted_model, global_step = utils.get_latest_checkpoint(targeted_model, log_dir, device)
+
+targeted_model.to(device=device)
+targeted_model.eval()
+model_num_labels = 4
+
+# MNIST train dataset and dataloader declaration
+# mnist_dataset = torchvision.datasets.MNIST('./dataset', train=True, transform=transforms.ToTensor(), download=True)
+# dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+
+data = acdc_data.load_and_maybe_process_data(
+        input_folder=exp_config.data_root,
+        preprocessing_folder=exp_config.preproc_folder,
+        mode=exp_config.data_mode,
+        size=exp_config.image_size,
+        target_resolution=exp_config.target_resolution,
+        force_overwrite=False,
+        split_test_train=True
+    )
+
+# the following are HDF5 datasets, not numpy arrays
+images_train = data['images_train']
+labels_train = data['masks_train']
+images_val = data['images_test']
+labels_val = data['masks_test']
+
+train_data = acdc_data.BasicDataset(images_train, labels_train)
+val_data = acdc_data.BasicDataset(images_val, labels_val)
+
+n_val = len(images_val)
+n_train = len(images_train)
+
+train_loader = DataLoader(train_data, batch_size=exp_config.batch_size, shuffle=True, num_workers=8, pin_memory=True)
+
+advGAN = AdvGAN_Attack(device,
+                       targeted_model,
+                       model_num_labels,
+                       image_nc,
+                       BOX_MIN,
+                       BOX_MAX,
+                       criterion,
+                       b)
+
+advGAN.train(train_loader, epochs)
